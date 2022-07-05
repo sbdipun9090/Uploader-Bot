0c9b2af7bdd8e0820f49735cc85df90e1c11ed42
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-07-05 13:42:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Client(RawClient, New):
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             plugins=dict(
-                root="bot/plugins"
+                root="plugins/plugins"
             )
         )
 
