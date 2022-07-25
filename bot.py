a545b31a3b02d2c3cf931d742a8251c637845d3a
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-07-25 12:24:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="Uploader")
-    Warrior = Tellyhub("@UPLOADER_X_BOT",
+    Tellybots = Tellyhub("@TellyUploaderProBot",
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     plugins=plugins)
-    Warrior.run()
+    Tellybots.run()
